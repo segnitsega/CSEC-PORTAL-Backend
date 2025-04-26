c48d89ef14237f90bad3c3c41dd0f6184805f025
segnitsega <segnitsega6@gmail.com>
2025-04-26 00:38:53 -0700

    update on eventsModel

--- src/models/eventsModel.ts
+++ src/models/eventsModel.ts
@@ -14,12 +14,12 @@ export interface eventsInterface extends Document {
 const eventSchema = new Schema<eventsInterface>({
     eventTitle: {type: String, required: true},
     division: {type: String},
-    groups: {type: [String]},
+    groups: {type: [String], default: undefined},
     eventDate: {type: String, required: true},
     startTime: {type: String, required: true},
     endTime: {type: String, required: true},
     visibility: {type: String, required: true},
-    attendance: {type: String}
+    attendance: {type: String, default: "Optional"}
 }) 
 
 export default model<eventsInterface>('Event', eventSchema) 
