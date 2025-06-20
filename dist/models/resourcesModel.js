c119f5d3b54a457db90b659f5c755712e84a8361
segnitsega <segnitsega6@gmail.com>
2025-06-20 15:04:11 +0300

    made .env private after project completion

--- dist/models/resourcesModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const resourceSchema = new mongoose_1.Schema({
-    resourceName: { type: String, required: true },
-    resourceLink: { type: String, required: true },
-    division: { type: String, required: true },
-});
-exports.default = (0, mongoose_1.model)("Resource", resourceSchema);
