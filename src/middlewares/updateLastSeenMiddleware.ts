8ad77158e59be15e6eb005534f3c1cd21610eb99
segnitsega <segnitsega6@gmail.com>
2026-01-06 23:19:50 +0300

    fix bug in update last seen middleware

--- src/middlewares/updateLastSeenMiddleware.ts
+++ src/middlewares/updateLastSeenMiddleware.ts
@@ -1,14 +1,32 @@
 import Member from "../models/membersModel";
 import { NextFunction, Request, Response } from "express";
 
-export const updateLastSeen = async(req: Request | any, res: Response, next: NextFunction) => {
-    try{
-        await Member.findOneAndUpdate(
-        { email: req.user.email }, 
-        { $set: {lastSeen: new Date} }
-    )
-    } catch(error){
-        console.log("Failed to update lastSeen", error)
-    }   
-    next()
-}
+const LAST_SEEN_THROTTLE_MS = 5 * 60 * 1000;
+
+const lastWriteByUser = new Map<string, number>();
+
+export const updateLastSeen = (req: Request | any, res: Response, next: NextFunction) => {
+    const email: string | undefined = req.user?.email;
+    if (!email) {
+        return next();
+    }
+
+    const now = Date.now();
+    const lastWrite = lastWriteByUser.get(email);
+
+    if (lastWrite && now - lastWrite < LAST_SEEN_THROTTLE_MS) {
+        return next();
+    }
+
+    lastWriteByUser.set(email, now);
+
+    Member.updateOne(
+        { email },
+        { $set: { lastSeen: new Date().toISOString() } }
+    ).catch((error) => {
+        console.log("Failed to update lastSeen", error);
+        lastWriteByUser.delete(email);
+    });
+
+    next();
+};
